9009aa1eaccdffa84d2d073a8787d164ab603e6f
tmelwig <119754693+tmelwig@users.noreply.github.com>
2025-03-28 13:39:18 +0000

    [Part 2] Add Personne Class

---
+++ tp4/Personne.cs
@@ -0,0 +1,18 @@
+﻿namespace tp4;
+
+
+class Personne
+{
+    public string Nom { get; set; }
+    public int Age { get; set; }
+
+    public Personne(string nom, int age) {
+        Nom = nom;
+        Age = age;
+    }
+
+    public void Hello(bool isLowerCase) {
+        string message = $"hello {Nom}, you are {Age}";
+        Console.WriteLine(isLowerCase ? message.ToLower() : message.ToUpper());
+    }
+}
---
+++ tp4/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace tp4;
+
+using Newtonsoft.Json;
+
+class Program
+{
+    static void Main()
+    {
+        Personne personne = new Personne("thomas", 24);
+        string stringifyPersonne = JsonConvert.SerializeObject(personne, Formatting.Indented);
+        personne.Hello(true);
+        Console.WriteLine(stringifyPersonne);
+    }
+}
